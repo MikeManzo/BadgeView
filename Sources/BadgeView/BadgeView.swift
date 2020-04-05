83b10fe9241229494cef91696aa6f00c41221e77
Mike Manzo <manzo.mike@gmail.com>
2020-04-05 11:08:30 -0400

    Error Checking

--- Sources/BadgeView/BadgeView.swift
+++ Sources/BadgeView/BadgeView.swift
@@ -260,7 +260,10 @@ public class BadgeView: NSView, CustomTaggable {
         //
         // Load the nib for the component then find and add the view from it.
         //
-        Bundle(for: type(of: self)).loadNibNamed("BadgeView", owner: self, topLevelObjects: &customViewTopLevelObjects)
+        if (!Bundle(for: type(of: self)).loadNibNamed("BadgeView", owner: self, topLevelObjects: &customViewTopLevelObjects)) {
+            print("BadgeView[loadNib]: Error Loading BadeView from NIB")
+            return
+        }
         
         let indexOfCustomView = customViewTopLevelObjects!.indexOfObject( passingTest: { (object:Any, index:Int, stop:UnsafeMutablePointer<ObjCBool>) -> Bool in
             return (object as AnyObject).isKind(of: NSView.self)
